a008fa95b9541d7b33b4f7c42cfc44aaf0067e3b
yallo428 <yallo428@naver.com>
2024-06-19 22:26:54 +0900

    :recycle: Position Entity와 연관관계 설정

--- src/main/java/org/example/develociraptor/domain/user/entity/User.java
+++ src/main/java/org/example/develociraptor/domain/user/entity/User.java
@@ -4,15 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.develociraptor.domain.position.entity.Position;
 import org.example.develociraptor.global.base.BaseEntity;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
@@ -28,6 +32,10 @@ public class User extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "position_id")
+	private Position position;
 
 	@Column(nullable = false, unique = true)
 	private String email;
