8aaf02dd4463e373617e7d9b90925cb8afcc25de
yallo428 <yallo428@naver.com>
2024-06-19 23:52:42 +0900

    :recycle: 필수 어노테이션 추가

--- src/main/java/org/example/develociraptor/domain/user/dto/UserRequestDto.java
+++ src/main/java/org/example/develociraptor/domain/user/dto/UserRequestDto.java
@@ -1,8 +1,14 @@
 package org.example.develociraptor.domain.user.dto;
 
 import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.develociraptor.domain.techstack.dto.TechStackRequestDto;
 
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class UserRequestDto {
 
     private String nickName;
