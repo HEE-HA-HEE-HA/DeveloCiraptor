58c80afb40334ceebde7730924576595f3f3d133
yallo428 <yallo428@naver.com>
2024-06-19 23:52:19 +0900

    :recycle: Long -> Short로 변경

--- src/main/java/org/example/develociraptor/domain/position/repository/PositionJpaRepository.java
+++ src/main/java/org/example/develociraptor/domain/position/repository/PositionJpaRepository.java
@@ -3,6 +3,6 @@ package org.example.develociraptor.domain.position.repository;
 import org.example.develociraptor.domain.position.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PositionJpaRepository extends JpaRepository<Position, Long> {
+public interface PositionJpaRepository extends JpaRepository<Position, Short> {
 
 }
--- src/main/java/org/example/develociraptor/global/exception/ErrorCode.java
+++ src/main/java/org/example/develociraptor/global/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-	MEMBER_NOT_FOUND("해당 유저를 찾을 수 없습니다.");
+	MEMBER_NOT_FOUND("해당 유저를 찾을 수 없습니다."),
+	POSITION_NOT_FOUND("해당 포지션이 존재하지 않습니다.");
 
 	private String message;
 
