91a631fd1a58df858e9424ed58d982d677eccff4
HEEEUN9812 <HEEEUN9812@gmail.com>
2024-06-24 02:01:27 +0900

    :sparkles: 팔로우 기능 생성

--- src/main/java/org/example/develociraptor/domain/follow/controller/FollowController.java
+++ src/main/java/org/example/develociraptor/domain/follow/controller/FollowController.java
@@ -2,6 +2,14 @@ package org.example.develociraptor.domain.follow.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.develociraptor.domain.follow.service.FollowService;
+import org.example.develociraptor.global.dto.ResponseDto;
+import org.example.develociraptor.global.security.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping("/{userId}/follow")
+    public ResponseEntity<ResponseDto<String>> createFollow(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable Long userId) {
+        String message = followService.createFollow(userDetails.getUserDetailsDto().id(), userId);
+
+        return ResponseDto.of(HttpStatus.CREATED, message);
+    }
 
 }
--- src/main/java/org/example/develociraptor/domain/follow/service/FollowService.java
+++ src/main/java/org/example/develociraptor/domain/follow/service/FollowService.java
@@ -1,6 +1,10 @@
 package org.example.develociraptor.domain.follow.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.develociraptor.domain.follow.entity.Follow;
+import org.example.develociraptor.domain.follow.repository.FollowJpaRepository;
+import org.example.develociraptor.domain.user.entity.User;
+import org.example.develociraptor.domain.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,4 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class FollowService {
 
+    private FollowJpaRepository followJpaRepository;
+    private UserService userService;
+
+    public String createFollow(Long fromId, Long toId) {
+        User fromUser =  userService.findById(fromId);
+        User toUser = userService.findById(toId);
+
+        Follow follow = Follow.builder()
+            .fromUser(fromUser)
+            .toUser(toUser)
+            .build();
+        followJpaRepository.save(follow);
+
+        return "팔로우 되었습니다.";
+    }
 }
