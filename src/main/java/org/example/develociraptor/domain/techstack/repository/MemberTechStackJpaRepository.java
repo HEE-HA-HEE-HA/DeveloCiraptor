3fd5bdf3d84c80e380d86b1635f5de3b05196327
yallo428 <116006875+yallo428@users.noreply.github.com>
2024-06-26 10:52:32 +0900

    유저 도메인 버그 해결 (#2)
    * :tada: init related user
    
    * :recycle: UserDetailsImpl안에 있는 User를 Dto로 변환
    
    * :recycle: Position Entity와 연관관계 설정
    
    * :recycle: id Long타입에서 Short로 변경
    
    * :recycle: UserId -> userId
    
    * :recycle: memberId -> userId
    
    * :recycle: positionId 타입 Long에서 Short로 변경
    
    * :recycle: memberId -> userId
    
    * :sparkles: ErrorCode 생성
    
    * :recycle: 인자값 변경
    
    * :recycle: 인자값 변경
    
    * :recycle: Long -> Short로 변경
    
    * :recycle: 필수 어노테이션 추가
    
    * :sparkles: 패스워드 유효성 검사 어노테이션 생성
    
    * :sparkles: 패스워드 유효성 검사 어노테이션 생성
    
    * :recycle: 기존 패스워드 유효성 어노테이션 -> 커스텀 어노테이션으로 변경
    
    * :sparkles: 패스워드 변경, 프로필 조회, 회원탈퇴 기능 구현
    
    * :wrench: PASSWORD_NOT_MATCH 메시지 추가
    
    * :recycle: ExceptionHandler 추가
    
    * :fire: MemberTechStack.java, MemberTechStackJpaRepository.java
    
    * :bug: 비밀번호가 암호화되면서 length가 20을 넘기면서 오류 발생. length 60으로 변경
    
    * :heavy_plus_sign: validation 추가
    
    * :fire: MemberRequestDto 삭제
    
    * :fire: 불필요한 코드 삭제
    
    * :bug: 유효성 검사 작동 확인 및 유저 소프트 딜리트 오류 해결

--- src/main/java/org/example/develociraptor/domain/techstack/repository/MemberTechStackJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.develociraptor.domain.techstack.repository;
-
-import org.example.develociraptor.domain.techstack.entity.MemberTechStack;
-import org.example.develociraptor.global.idclass.MemberTechStackId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberTechStackJpaRepository extends JpaRepository<MemberTechStack, MemberTechStackId> {
-
-}
--- src/main/java/org/example/develociraptor/domain/user/entity/User.java
+++ src/main/java/org/example/develociraptor/domain/user/entity/User.java
@@ -25,7 +25,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE member SET is_deleted = true where member_id = ?")
+@SQLDelete(sql = "UPDATE User SET is_deleted = true where user_id = ?")
 @SQLRestriction("is_deleted = false")
 public class User extends BaseEntity {
 
@@ -45,8 +45,7 @@ public class User extends BaseEntity {
 	@Column(nullable = false, unique = true, length = 12)
 	private String nickName;
 
-	@Size(min = 4, max = 20)
-	@Column(nullable = false, length = 20)
+	@Column(nullable = false, length = 60)
 	private String password;
 
 	private String introduction;
@@ -69,8 +68,8 @@ public class User extends BaseEntity {
 		this.position = position;
 	}
 
-	public boolean isPasswordCheck(PasswordEncoder passwordEncoder, String s) {
-		return passwordEncoder.matches(password, s);
+	public boolean isPasswordCheck(PasswordEncoder passwordEncoder, String password) {
+		return passwordEncoder.matches(password, this.password);
 	}
 
 	public void updatePassword(String newPassword) {
